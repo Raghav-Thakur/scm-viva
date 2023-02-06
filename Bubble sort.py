f99bc77899a3d4322d36f6b4a6caacf10e5bb3ea
Raghav Thakur <122594195+Raghav-Thakur@users.noreply.github.com>
2023-02-06 13:47:42 +0530

    file added
    fd

---
+++ Bubble sort.py
@@ -0,0 +1,16 @@
+def bubblesort(li):
+    swapped = False
+    a=len(li)
+    for n in range(a-1, 0, -1):
+        for i in range(n):
+            if li[i] > li[i + 1]:
+                swapped = True
+                li[i], li[i + 1] = li[i + 1], li[i]       
+        if not swapped:
+            return
+
+print("Enter Your Unsorted List: ")
+li = [int(x) for x in input().split()]
+bubblesort(li)
+print("Sorted List is: ")
+print(li)
